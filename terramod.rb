179dc2fe6b79583db1dd1973370ceaec5f13ec9d
Hayden Parker <haydenkparker@gmail.com>
2015-02-21 23:16:06 -0800

    Working on event reciever

--- terramod.rb
+++ terramod.rb
@@ -2,28 +2,47 @@
 
 require 'sinatra/base'
 
-
 class EventReciever < Sinatra::Base
 	# recieve requests from the nexus and send them to callbacks for the apps
 	
 	configure do
-		# set globally avaliable event queues for callbacks?
+		# access database
 	end
 	
-	post '/add_modules' do
-		# Called when a nexus comes online
-		# insert into database the presence of this nexus
+	post '/event_reciever' do
+		event = JSON.parse request.body.read
+		type = event['type']
+		uuid = event['uuid']
+		data = event['data']
+		if type == "ModuleReport"
+			#settings.db.execute "DROP TABLE ?;" [uuid]
+			#modules = JSON.parse data
+			#settings.db.execute "CREATE TABLE ?(uuid TEXT, name TEXT, class TEXT, room TEXT, UNIQUE(uuid));" [uuid]
+			##settings.db.execute "INSERT INTO nexus VALUES(?,?);" [uuid, request.ip]
+			#modules.each do |k, v|
+				#module_uuid = k
+				#name = v['name']
+				#type = v['type']
+				#room = v['room']
+				#settings.db.execute "INSERT INTO ? VALUES(?,?,?,?);", [uuid, name, type, room]
+			#end
+		elsif type == "EventReport"
+			#callbacks = settings.db.execute "SELECT method FROM callbacks WHERE module_uuid = ?;" [uuid]
+			#callbacks.each do |callback|
+				#method = Module.const_get(callback[0])
+				#method(data)
+			#end
+		end
 	end
+	
 end
 
 class Controller < Sinatra::Base
-	# Present the web interface and API
 
 	configure do
-		#db_flie = "/home/hayden/Downloads/controller.db"
-		#File.delete(db_flie) if File.exist?(db_flie)
+		#db_flie = "/./terramod.db"
 		#db = SQLite3::Database.new db_flie
-		#db.execute "CREATE TABLE Nexus(uuid TEXT, name TEXT, class TEXT, room TEXT, hardware TEXT, UNIQUE(uuid));"
+		#db.execute "CREATE TABLE nexus(uuid TEXT, ip TEXT, UNIQUE(uuid));"
 		
 	end
 
@@ -34,4 +53,3 @@ class Controller < Sinatra::Base
 end
 
 
-Controller.run!
